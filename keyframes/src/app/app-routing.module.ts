a31e90bbb2a00343788ff53b770764093ab93e6c
colonnam <colonna_m15@yahoo.com>
2021-12-16 17:14:10 +0100

    front 1.1

--- keyframes/src/app/app-routing.module.ts
+++ keyframes/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { KeyframesComponent } from './keyframes/keyframes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/keyframes', pathMatch: 'full' },
+  { path: 'keyframes', component: KeyframesComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- keyframes/src/app/keyframes/keyframes.component.ts
+++ keyframes/src/app/keyframes/keyframes.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSlider } from '@angular/material/slider';
+
 
 @Component({
   selector: 'app-keyframes',
@@ -7,9 +9,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class KeyframesComponent implements OnInit {
 
+  max_frames=100;
+
+  images= [
+  {'name': 'img1271.jpg',
+    'start':0,
+    'end':25},
+  {'name':'img2542.jpg',
+  'start':25,
+    'end':50},
+  {'name':'img3813.jpg',
+    'start':50,
+      'end':75},
+  {'name':'img5084.jpg',
+      'start':75,
+        'end':100}
+];
+current_img=this.images[0];
+  
   constructor() { }
 
   ngOnInit(): void {
+    
+
+  }
+  showVal(val:any){
+    console.log(val.value)
+    for (let img of this.images){
+      if(val.value>=img.start && val.value<img.end)
+        {this.current_img=img;
+        break;}
+    }
   }
 
 }
