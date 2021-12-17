03704a41232cc622f29ded1a857cd1b333401a1e
Jean Bertrand <90519788+Jean2B@users.noreply.github.com>
2021-12-17 09:28:10 +0100

    Update keytest.py

--- keytest.py
+++ keytest.py
@@ -3,23 +3,23 @@ import json
 from flask import Flask, jsonify, request
 
 file = 'video.mp4'
+nb_keyframes = 5
 
-#5 images
 cap= cv2.VideoCapture(file)
 frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
 nom_video = '"nom_video":"' + file + '",'
 nb_frame='"nb_frame":' + str(frame_count) + ','
 
 images = '"images":['
-sep_frame = int(frame_count / 5)
+sep_frame = int(frame_count / nb_keyframes)
 i=1
 while(cap.isOpened()):
-    
+
     ret, frame = cap.read()
     if ret == False:
         break
     if i%sep_frame==0:
-        cv2.imwrite('5images/img'+str(i)+'.jpg',frame)
+        cv2.imwrite('images/img'+str(i)+'.jpg',frame)
         images += '{"nom_img":"img'+str(i)+'.jpg","debut":'+str(i)+',"fin":'+str(i+sep_frame)+'},'
     i+=1
 
@@ -37,31 +37,5 @@ def get_json():
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=5000, debug=True)
 
-#10 images
-cap= cv2.VideoCapture('video.mp4')
-frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-sep_frame = int(frame_count / 10)
-i=1
-while(cap.isOpened()):
-    ret, frame = cap.read()
-    if ret == False:
-        break
-    if i%sep_frame==0:
-        cv2.imwrite('10images/img'+str(i)+'.jpg',frame)
-    i+=1
-
-#20 images
-cap= cv2.VideoCapture('video.mp4')
-frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-sep_frame = int(frame_count / 20)
-i=1
-while(cap.isOpened()):
-    ret, frame = cap.read()
-    if ret == False:
-        break
-    if i%sep_frame==0:
-        cv2.imwrite('20images/img'+str(i)+'.jpg',frame)
-    i+=1
- 
 cap.release()
 cv2.destroyAllWindows()
